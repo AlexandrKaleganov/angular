3fd96bd346862adf5359f22ef1d76a8edb3665a2
Alexander Kaleganov <alexmur07@mail.ru>
2020-04-12 14:29:42 +0700

    6. Стандартные структурные директивы [#62795]

--- task-planner/src/app/task-list/task-list.component.ts
+++ task-planner/src/app/task-list/task-list.component.ts
@@ -6,30 +6,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
- taskModels: any = [];
-  constructor() { }
+  taskModels: any = [];
+  taskFilter: boolean = true;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.initTaskModel();
   }
+
   initTaskModel() {
     this.taskModels.push(
-      {id:'1', name:'task1', category: 'category1', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Завершена'},
-      {id:'2', name:'task2', category: 'category2', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Запланирована'},
-      {id:'3', name:'task3', category: 'category3', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Просрочена'},
-      {id:'4', name:'task4', category: 'category4', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Запланирована'},
-      {id:'5', name:'task5', category: 'category5', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Завершена'},
-      {id:'6', name:'task6', category: 'category6', dateStart:'22:15 05.06.2020', dateEnd:'22:15 06.06.2020', status:'Завершена'});
+      {
+        id: '1',
+        name: 'task1',
+        category: 'category1',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Завершена'
+      },
+      {
+        id: '2',
+        name: 'task2',
+        category: 'category2',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Запланирована'
+      },
+      {
+        id: '3',
+        name: 'task3',
+        category: 'category3',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Просрочена'
+      },
+      {
+        id: '4',
+        name: 'task4',
+        category: 'category4',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Запланирована'
+      },
+      {
+        id: '5',
+        name: 'task5',
+        category: 'category5',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Завершена'
+      },
+      {
+        id: '6',
+        name: 'task6',
+        category: 'category6',
+        dateStart: '22:15 05.06.2020',
+        dateEnd: '22:15 06.06.2020',
+        status: 'Завершена'
+      });
   }
+
   addTask() {
     console.log('add Task');
   }
 
-  setStatus(object: any) {
-    object.status = !object.status;
+  setStatus(object: any, $event) {
+    if ($event.target.checked) {
+      object.status = 'Завершена';
+    } else {
+      object.status = 'Запланирована';
+    }
   }
+
   filterTasks($event) {
-    console.log($event.target.checked);
+    this.taskFilter = $event.target.checked;
+  }
+
+  isViews(task: any) {
+    if (this.taskFilter) {
+      return task.status === 'Завершена';
+    } else {
+      return true;
+    }
   }
 
   deleteTask(tas: any) {
@@ -39,8 +99,8 @@ export class TaskListComponent implements OnInit {
   getTaskListsSize() {
     return this.taskModels?.length;
   }
+
   getTasksAmountByStatus(status: string) {
     return this.taskModels.filter(task => task.status === status)?.length;
-
-}
+  }
 }
